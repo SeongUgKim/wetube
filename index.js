ebfaf3c5c9733187fa3308076d216b6f4fb46371
SeongUgKim <59695647+SeongUgKim@users.noreply.github.com>
2020-07-17 21:34:17 +0900

    Third Commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
@@ -7,4 +7,14 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 };
 
+const handleHome = (req, res) => {
+  res.send("Hello from home");
+};
+
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
