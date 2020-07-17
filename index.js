f57908c2061e1104515441e40cc0e380e16b4246
SeongUgKim <59695647+SeongUgKim@users.noreply.github.com>
2020-07-17 22:05:16 +0900

    Fourth Commits

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -14,6 +18,11 @@ const handleHome = (req, res) => {
 const handleProfile = (req, res) => {
   res.send("You are on my profile");
 };
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
