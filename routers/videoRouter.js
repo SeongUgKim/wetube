1d38063fc40d31b3a5ed18c505a105fc7397fe57
SeongUgKim <59695647+SeongUgKim@users.noreply.github.com>
2020-07-20 02:26:25 +0900

    Fifth Commits

---
+++ routers/videoRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+import routes from "../routes";
+import {
+  videos,
+  videoDetail,
+  upload,
+  editVideo,
+  deleteVideo,
+} from "../controllers/videoController";
+import { editProfile } from "../controllers/userController";
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
+export default videoRouter;
